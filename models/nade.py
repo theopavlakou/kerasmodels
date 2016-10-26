1734781b6b70d1cc6a6cd587ba1a960d210a00bf
Theo Pavlakou <theo.pavlakou@gmail.com>
2016-10-26 11:13:35 +0100

    Adds RNADE and tests

--- models/nade.py
+++ models/nade.py
@@ -1,12 +1,15 @@
 from __future__ import division
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.engine.topology import Layer
+from keras.layers import Input, merge
 from keras.initializations import glorot_normal
 from keras.layers import Activation
 from keras import backend as K
 from theano import tensor as T
+import numpy as np
 import theano
 from theano.tensor.shared_randomstreams import RandomStreams
+from common_methods.vector_methods import logsumexp
 
 
 class NadeInputLayer(Layer):
@@ -74,6 +77,109 @@ class NadeOutputBinaryLayer(Layer):
         return input_shape[0], self.output_dim
 
 
+class RNadeOutputLayer(Layer):
+    """
+    A class that implements a hidden-to-output layer for RNADE, as outlined in
+    https://arxiv.org/abs/1306.0186. This runs in O(HKD) time.
+    """
+    def __init__(self, output_dim, number_components_per_conditional, input_var, weight_init=glorot_normal, **kwargs):
+        self.output_dim = output_dim
+        self.num_components = number_components_per_conditional
+        self.weight_init = weight_init
+        self.input_var = input_var
+        super(RNadeOutputLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 2
+        input_dim = input_shape[1]
+
+        self.V_alpha = self.weight_init((self.num_components, input_dim, self.output_dim), name="V_alpha")
+        self.V_mu = self.weight_init((self.num_components, input_dim, self.output_dim), name="V_mu")
+        self.V_sigma = self.weight_init((self.num_components, input_dim, self.output_dim), name="V_sigma")
+        self.b_alpha = self.weight_init((self.num_components, self.output_dim), name="b_alpha")
+        self.b_mu = self.weight_init((self.num_components, self.output_dim), name="b_mu")
+        self.b_sigma = self.weight_init((self.num_components, self.output_dim), name="b_sigma")
+
+        self.trainable_weights = [self.V_alpha, self.V_mu, self.V_sigma,
+                                  self.b_alpha, self.b_mu, self.b_sigma]
+
+    def get_log_alpha(self, x):
+        # (N, 1, H, D)
+        h_new_shape = x.dimshuffle(0, 'x', 1, 2)
+        # ((N, 1, H, D) (1, K, H, D)).sum(axis=2) => (N, K, D)
+        activation_alpha = (h_new_shape * self.V_alpha).sum(2)
+        # (N, K, D). Parameters to the mixture of Gaussians for each dimension.
+        exponent_alpha = self.b_alpha + activation_alpha
+        # We subtract the max because by doing so we avoid overflows and the ratio for softmax is left invariant.
+        exponent_alpha_minus_max = exponent_alpha - exponent_alpha.max(axis=1).dimshuffle(0, 'x', 1)
+        # (N, K, D) <= (N, K, D) - (N, 1, D)
+        return exponent_alpha_minus_max - K.log(K.sum(K.exp(exponent_alpha_minus_max), axis=1)).dimshuffle(0, 'x', 1)
+
+    def get_mu(self, x):
+        h_new_shape = x.dimshuffle(0, 'x', 1, 2)
+        activation_mu = K.sum(h_new_shape * self.V_mu, axis=2)
+        # (N, K, D). Parameters to the mixture of Gaussians for each dimension.
+        return self.b_mu + activation_mu
+
+    def get_log_sigma(self, x):
+        h_new_shape = x.dimshuffle(0, 'x', 1, 2)
+        activation_sigma = K.sum(h_new_shape * self.V_sigma, axis=2) + self.b_sigma
+        # We clip to ensure that we don't get numerical errors.
+        return K.clip(activation_sigma, -20, 20)
+
+    def call(self, x, mask=None):
+        log_alpha = self.get_log_alpha(x)
+        mu = self.get_mu(x)
+        log_sigma = self.get_log_sigma(x)
+
+        sigma = K.exp(log_sigma)
+
+        # (N, K, D)
+        log_p_z_x_joint = (-1/(2*K.square(sigma))*K.square(self.input_var.dimshuffle(0, 'x', 1) - mu) + log_alpha -
+                          1/2*K.log(2*np.pi) - log_sigma)
+
+        return K.sum(logsumexp(log_p_z_x_joint, axis=1), axis=1)
+
+    def get_output_shape_for(self, input_shape):
+        return input_shape[0], self.output_dim
+
+
+class RNade(Model):
+
+    def __init__(self, input_dim, hidden_dim, number_components_per_conditional, weight_init=glorot_normal,
+                 nonlinearity=K.relu, name="RNade"):
+        """
+        Initialise RNADE, as is outlined in the following paper:
+        https://arxiv.org/abs/1306.0186
+
+        :param input_dim: The input dimensionality.
+        :param hidden_dim: The hidden layer dimensionality.
+        :param number_components_per_conditional:   the number of components to use in
+                                                    each conditional distribution.
+        :param nonlinearity: The non-linearity to use at the hidden layer.
+        :param name: The name of the specific NADE instance.
+        """
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.num_components = number_components_per_conditional
+        self.nonlinearity = nonlinearity
+
+        input = Input(shape=(self.input_dim, ))
+        pre_activation = NadeInputLayer(self.hidden_dim, self.input_dim, weight_init=weight_init)(input)
+        hidden_layer = Activation(self.nonlinearity)(pre_activation)
+
+        output = RNadeOutputLayer(self.input_dim, self.num_components, input_var=input, weight_init=weight_init)(hidden_layer)
+        super(RNade, self).__init__(input=input, output=output, name=name)
+
+    @staticmethod
+    def cost(x, log_probability_density):
+        return -log_probability_density.mean()
+
+    def sample(self, N):
+        # TODO
+        pass
+
+
 class BinaryNade(Sequential):
 
     def __init__(self, input_dim, hidden_dim, nonlinearity=K.relu, name="BinaryNade"):
@@ -108,12 +214,12 @@ class BinaryNade(Sequential):
         Returns the negative log-likelihood of the parameters given the
         input data.
 
-        :param x: (N, D) tensor, where N is the number of examples and D is the 
+        :param x: (N, D) tensor, where N is the number of examples and D is the
                   dimension of the input data.
-        :param prob_x: (N, D) tensor, where the dth dimension in each case is 
-                       the conditional probability of the dth element of the 
-                       input being one given all the previous d-1 elements. 
-        :return: The mean negative log likelihood of the data. 
+        :param prob_x: (N, D) tensor, where the dth dimension in each case is
+                       the conditional probability of the dth element of the
+                       input being one given all the previous d-1 elements.
+        :return: The mean negative log likelihood of the data.
         """
         return -K.mean(K.sum((x * K.log(prob_x) + (1 - x) * K.log(1 - prob_x)), axis=1))
 
